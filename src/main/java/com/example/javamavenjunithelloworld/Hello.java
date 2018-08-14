f49acdd02ff8cc5129d99ce5505ee919ae8d9a35
vignard <olivier.vignard@gemalto.com>
2018-08-14 12:39:28 +0200

    last

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 
     static final String HELLO = "Hello!";
 
-    int MAXIMUM_AMOUNT_OF_TIMES = 20;
+    public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
 
     private int times = 1;
 
@@ -17,7 +17,7 @@ public class Hello {
      * @param times How many times should this class say "Hello!"? The value should be no larger than 20.
      * @throws IllegalArgumentException Thrown when times is larger than 20 or a negative number.
      */
-    public void setTimes(int times) {
+    public setTimes(int times) {
         if (times < 0 || times > MAXIMUM_AMOUNT_OF_TIMES) {
             throw new IllegalArgumentException(String.format(
                     "Parameter «times» should be a positive number no larger than %d.",
